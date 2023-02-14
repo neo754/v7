384e3b90a00f3c1b5dcee8e155ded6cbbc7b62dc
neo754 <115906106+neo754@users.noreply.github.com>
2023-02-14 19:38:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -291,11 +291,11 @@ global.imagebot = 'https://telegra.ph/file/7e11da809594b30705ef4.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 global.thumbs = ['https://telegra.ph/file/7e11da809594b30705ef4.jpg']
 global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+  'https://telegra.ph/file/3bd379236dcc367324384.jpg', 'https://telegra.ph/file/3bd379236dcc367324384.jpg',
+  'https://telegra.ph/file/3bd379236dcc367324384.jpg', 'https://telegra.ph/file/3bd379236dcc367324384.jpg',
+  'https://telegra.ph/file/3bd379236dcc367324384.jpg', 'https://telegra.ph/file/3bd379236dcc367324384.jpg',
+  'https://telegra.ph/file/3bd379236dcc367324384.jpg', 'https://telegra.ph/file/3bd379236dcc367324384.jpg',
+  'https://telegra.ph/file/3bd379236dcc367324384.jpg', 'https://telegra.ph/file/3bd379236dcc367324384.jpg'
 ]
 global.fotonya1 = 'https://telegra.ph/file/7e11da809594b30705ef4.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/7e11da809594b30705ef4.jpg' //ini juga ganti 
